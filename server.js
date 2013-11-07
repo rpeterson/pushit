553d69b59193219455220641272d0f615629d2d3
Ryan Peterson <ryan@inovat.com>
2013-11-07 10:45:20 -0500

    Update

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var ZMQ = require('zmq');
 var Socket = ZMQ.socket('push');
 var Gith = require('gith').create( config.web );
 var MsgPack = require('msgpack');
+var Express = require('express');
+var App = Express();
 
 Socket.bindSync('tcp://*:' + config.zmq);
 
@@ -11,7 +13,14 @@ console.log('ZeroMQ ' + config.zmq);
 console.log('Githook ' + config.web);
 
 Gith({
-  repo: '(.*)'
+  repo: '.*'
 }).on( 'all', function( payload ) {
   Socket.send(MsgPack.pack(payload));
 });
+
+// Web App
+App.get('/', function(req, res){
+  res.send('Push It. Push It Real Good.');
+});
+
+App.listen(config.port);
